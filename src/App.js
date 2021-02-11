e8b4b0285a7f69b760226a7c92f0e0dbdbf99d88
Aleksandr <aleksandr@gmail.com>
2021-02-11 13:59:03 +0200

    App Aloha

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import Greeting from './components/Greeting/index'
+import Aloha from './components/Aloha/index'
 
 function App() {
+  const user = {
+    firstName: "Sasha",
+    url: 'http://placehold.it/100'
+  }
+
+  const user1 = {
+    firstName: "Masha",
+    url: 'http://placehold.it/100'
+  }
+
+  const user2 = {
+    firstName: "Dasha",
+    url: 'http://placehold.it/100'
+  }
+
   return (
     <>
-      <Greeting name="Vlad" />
-      <Greeting name="Masha" />
-      <Greeting name="Dasha" />
+      <Aloha name={user.firstName} photo={user.url} />
+      <Aloha name={user1.firstName} photo={user1.url} />
+      <Aloha name={user2.firstName} photo={user2.url} />
     </>
   )
 }
