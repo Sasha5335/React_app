a26842a01dad6222c6915e1084fab80604b12528
Aleksandr <aleksandr@gmail.com>
2021-02-24 16:04:34 +0200

    React router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import WindowSize from './components/WindowSize'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 
-function App(props) {
+const App = (props) => {
   return (
     <>
-      <WindowSize />
+      <BrowserRouter>
+
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contacts'>Contacts</Link></li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route path='/contacts'>
+            <Contacts />
+          </Route>
+        </Switch>
+
+      </BrowserRouter>
     </>
   );
 }
 
-
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const Contacts = () => <div>Contacts</div>;
 export default App;
